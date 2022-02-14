e115453af5d2dbe4448e07957a24c399da2fc53c
Jonah Zorniger <jonahzorniger8@gmail.com>
2022-02-13 21:21:40 -0500

    Login working

---
+++ JobTracker.Data/Job.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.Data
+{
+    public class Job
+    {
+        [Key]
+        public int JobId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public int SoldAmount { get; set; }
+
+        [Required]
+        public int Earnings { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset ModifiedUtc { get; set; }
+
+
+    }
+}

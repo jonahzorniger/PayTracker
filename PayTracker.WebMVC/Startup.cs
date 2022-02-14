0514acb18385efd7be509cbb17f87ab8fdf589fb
Jonah Zorniger <jonahzorniger8@gmail.com>
2022-02-13 20:26:05 -0500

    Add project files.

---
+++ PayTracker.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PayTracker.WebMVC.Startup))]
+namespace PayTracker.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

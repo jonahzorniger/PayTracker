6a399fa55a2545704f62242fd1aad7698dda2185
Jonah Zorniger <jonahzorniger8@gmail.com>
2022-02-13 22:26:58 -0500

    added Get Job Index

---
+++ JobTracker.Data/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.Data
+{
+    public class Customer
+    {
+        [Key]
+       public int CustomerId { get; set; }
+
+        [Required,
+            MaxLength(100),
+                MinLength(1)]
+       public string FirstName { get; set; }
+
+        [Required, 
+            MaxLength(100),
+                MinLength(1)]
+       public string LastName { get; set; }
+    }
+}
---
+++ JobTracker.Data/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.Data
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required,
+            MaxLength(100),
+                MinLength(1)]
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        [Required,
+            MaxLength(100),
+                MinLength(1)]
+        public decimal Price { get; set; }
+
+    }
+}
